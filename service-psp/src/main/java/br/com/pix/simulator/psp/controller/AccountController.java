70662196c058489afe20ee7d4d421e753333451a
Jusley Souza <jusleysouza31@gmail.com>
2025-10-26 17:19:45 -0300

    feat(account-controller) Creating and implementing the AccountController and the createAccount route

---
+++ service-psp/src/main/java/br/com/pix/simulator/psp/controller/AccountController.java
@@ -0,0 +1,29 @@
+package br.com.pix.simulator.psp.controller;
+
+import br.com.pix.simulator.psp.dto.account.AccountCreateRequest;
+import br.com.pix.simulator.psp.dto.account.AccountResponse;
+import br.com.pix.simulator.psp.service.AccountService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/accounts")
+public class AccountController {
+
+    private final AccountService accountService;
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @PostMapping
+    public ResponseEntity<AccountResponse> createAccount(@Valid @RequestBody AccountCreateRequest request ) {
+        return new ResponseEntity<>(accountService.createAccount(request), HttpStatus.CREATED);
+    }
+
+}
