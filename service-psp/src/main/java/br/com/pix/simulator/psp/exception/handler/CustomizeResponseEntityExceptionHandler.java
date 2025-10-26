65046120a823503dc161643a8e2dfc76d7d7be90
Jusley Souza <jusleysouza31@gmail.com>
2025-10-26 19:01:01 -0300

    feat(custom-exception-handler) creation and implementation of general class for custom exception

---
+++ service-psp/src/main/java/br/com/pix/simulator/psp/exception/handler/CustomizeResponseEntityExceptionHandler.java
@@ -0,0 +1,52 @@
+package br.com.pix.simulator.psp.exception.handler;
+
+import br.com.pix.simulator.psp.dto.error.FieldError;
+import br.com.pix.simulator.psp.dto.error.ResponseError;
+import br.com.pix.simulator.psp.exception.ExceptionResponse;
+import br.com.pix.simulator.psp.exception.ResourceNotFoundException;
+import br.com.pix.simulator.psp.exception.ValidationException;
+import lombok.Generated;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Generated
+@RestControllerAdvice
+public class CustomizeResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<ExceptionResponse> handleAllExceptions(Exception exception, WebRequest request){
+        ExceptionResponse exceptionResponse = new ExceptionResponse(
+                new Date(), exception.getMessage(), request.getDescription(false));
+        return new  ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR); //500
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public final ResponseEntity<ExceptionResponse> handleNotFoundExceptions(Exception exception, WebRequest request){
+        ExceptionResponse exceptionResponse = new ExceptionResponse(
+                new Date(), exception.getMessage(), request.getDescription(false));
+        return new  ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND); //404
+    }
+
+    @ExceptionHandler(ValidationException.class)
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        List<FieldError> errors = ex.getBindingResult().getFieldErrors().stream()
+                .map(fieldError -> new FieldError(fieldError.getField(), fieldError.getDefaultMessage()))
+                .collect(Collectors.toList());
+        ResponseError responseError = new ResponseError(errors);
+        return new ResponseEntity<>(responseError, HttpStatus.UNPROCESSABLE_ENTITY); // 422
+    }
+
+}
