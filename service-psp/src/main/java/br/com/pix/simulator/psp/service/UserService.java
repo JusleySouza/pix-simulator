ce3696a9edb53e24501ad7791a8576ee488e8feb
Jusley Souza <jusleysouza31@gmail.com>
2025-10-26 19:20:57 -0300

    feat(user-service) Creating and implementing of the searchUserById service

--- service-psp/src/main/java/br/com/pix/simulator/psp/service/UserService.java
+++ service-psp/src/main/java/br/com/pix/simulator/psp/service/UserService.java
@@ -2,10 +2,14 @@ package br.com.pix.simulator.psp.service;
 
 import br.com.pix.simulator.psp.dto.user.UserCreateRequest;
 import br.com.pix.simulator.psp.dto.user.UserResponse;
+import br.com.pix.simulator.psp.exception.ResourceNotFoundException;
 import br.com.pix.simulator.psp.model.User;
 import br.com.pix.simulator.psp.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
 
 @Service
 public class UserService {
@@ -35,6 +39,15 @@ public class UserService {
                 savedUser.getName(),
                 savedUser.getCpf()
         );
+    }
+
+    @Transactional(readOnly = true)
+    public UserResponse searchUserById(UUID userId) {
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found with ID: " + userId));
+
+        return new UserResponse(user.getUserId(), user.getName(), user.getCpf());
     }
 
 }
