240ef18199e36e169f65dd2456e62d7899de3646
Jusley Souza <jusleysouza31@gmail.com>
2025-10-26 17:32:18 -0300

    feat(user-controller) Creating and implementing the UserController class and the createUser route

---
+++ service-psp/src/main/java/br/com/pix/simulator/psp/controller/UserController.java
@@ -0,0 +1,29 @@
+package br.com.pix.simulator.psp.controller;
+
+import br.com.pix.simulator.psp.dto.user.UserCreateRequest;
+import br.com.pix.simulator.psp.dto.user.UserResponse;
+import br.com.pix.simulator.psp.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<UserResponse> createUser(@Valid @RequestBody UserCreateRequest request) {
+        return new ResponseEntity<>(userService.createUser(request), HttpStatus.CREATED);
+    }
+
+}
