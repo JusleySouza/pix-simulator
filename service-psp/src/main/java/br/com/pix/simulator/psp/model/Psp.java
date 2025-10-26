52425dccb743aafe5726f3828f159f2a91df0eef
Jusley Souza <jusleysouza31@gmail.com>
2025-10-26 01:48:07 -0300

    feat(psp) creation and implementation of the Psp model class

---
+++ service-psp/src/main/java/br/com/pix/simulator/psp/model/Psp.java
@@ -0,0 +1,26 @@
+package br.com.pix.simulator.psp.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "psps")
+public class Psp {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true, length = 3)
+    private String bankCode;
+}
