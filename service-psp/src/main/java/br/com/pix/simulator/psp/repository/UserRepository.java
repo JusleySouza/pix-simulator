01612fef634374d457fb0b54989cf030a0a47359
Jusley Souza <jusleysouza31@gmail.com>
2025-10-26 17:34:18 -0300

    feat(user-repository) Creation of the UserRepository interface and the findByCpf query

--- service-psp/src/main/java/br/com/pix/simulator/psp/repository/UserRepository.java
+++ service-psp/src/main/java/br/com/pix/simulator/psp/repository/UserRepository.java
@@ -1,10 +1,13 @@
 package br.com.pix.simulator.psp.repository;
 
-import br.com.pix.simulator.psp.exception.InsufficientBalanceException;
 import br.com.pix.simulator.psp.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.math.BigDecimal;
+import java.util.Optional;
 import java.util.UUID;
 
-public interface UserRepository extends JpaRepository<User, UUID> { }
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByCpf(String cpf);
+
+}
