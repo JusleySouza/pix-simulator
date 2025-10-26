75745ac0ae717f532831b4a0ab5cdd8999765c00
Jusley Souza <jusleysouza31@gmail.com>
2025-10-26 17:26:03 -0300

    feat(psp-controller) Creating and implementing the PspController and the createPsp route

---
+++ service-psp/src/main/java/br/com/pix/simulator/psp/controller/PspController.java
@@ -0,0 +1,29 @@
+package br.com.pix.simulator.psp.controller;
+
+import br.com.pix.simulator.psp.dto.psps.PspCreateRequest;
+import br.com.pix.simulator.psp.dto.psps.PspResponse;
+import br.com.pix.simulator.psp.service.PspService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/psps")
+public class PspController {
+
+    private final PspService pspService;
+
+    public PspController(PspService pspService) {
+        this.pspService = pspService;
+    }
+
+    @PostMapping
+    public ResponseEntity<PspResponse> createPsp(@Valid @RequestBody PspCreateRequest request) {
+        return new ResponseEntity<>(pspService.createPsp(request), HttpStatus.CREATED);
+    }
+
+}
