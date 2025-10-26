2287faed9c5f642b22ab3d281105967068bf93b5
Jusley Souza <jusleysouza31@gmail.com>
2025-10-26 19:20:02 -0300

    feat(user-controller) Creating and implementing of the searchUser route

--- service-psp/src/main/java/br/com/pix/simulator/psp/controller/UserController.java
+++ service-psp/src/main/java/br/com/pix/simulator/psp/controller/UserController.java
@@ -6,10 +6,9 @@ import br.com.pix.simulator.psp.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -24,6 +23,11 @@ public class UserController {
     @PostMapping
     public ResponseEntity<UserResponse> createUser(@Valid @RequestBody UserCreateRequest request) {
         return new ResponseEntity<>(userService.createUser(request), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserResponse> searchUser(@PathVariable UUID userId) {
+        return ResponseEntity.ok(userService.searchUserById(userId));
     }
 
 }
