051bf88ff86e5e5815e85b370eec6f3de7aa1368
Jusley Souza <jusleysouza31@gmail.com>
2025-10-26 01:49:32 -0300

    feat(account) creation and implementation of the Bank Account model class

---
+++ service-psp/src/main/java/br/com/pix/simulator/psp/model/Account.java
@@ -0,0 +1,39 @@
+package br.com.pix.simulator.psp.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "contas")
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID accountId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "psp_id", nullable = false)
+    private Psp psp;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "usuario_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false, length = 4)
+    private String agency;
+
+    @Column(nullable = false, length = 8)
+    private String accountNumber;
+
+    @Column(nullable = false, precision = 19, scale = 2)
+    private BigDecimal balance;
+
+}
