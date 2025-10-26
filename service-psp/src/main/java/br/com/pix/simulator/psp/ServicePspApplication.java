c35619213f88f1ab21d32cd50aa2c9c376d76658
Jusley Souza <jusleysouza31@gmail.com>
2025-10-25 23:10:28 -0300

    feat(main) Adding the main class

---
+++ service-psp/src/main/java/br/com/pix/simulator/psp/ServicePspApplication.java
@@ -0,0 +1,13 @@
+package br.com.pix.simulator.psp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicePspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicePspApplication.class, args);
+	}
+
+}
