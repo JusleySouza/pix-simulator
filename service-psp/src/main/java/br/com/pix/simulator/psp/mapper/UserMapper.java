e82a49a07cf3246a98e56c4a744b2c0bd11bcc64
Jusley Souza <jusleysouza31@gmail.com>
2025-10-30 18:04:05 -0300

    feat(user-mapper) Creating and implementing the UserMapper mapping class

---
+++ service-psp/src/main/java/br/com/pix/simulator/psp/mapper/UserMapper.java
@@ -0,0 +1,41 @@
+package br.com.pix.simulator.psp.mapper;
+
+import br.com.pix.simulator.psp.dto.user.UserCreateRequest;
+import br.com.pix.simulator.psp.dto.user.UserResponse;
+import br.com.pix.simulator.psp.model.User;
+import lombok.Generated;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+@Generated
+public class UserMapper {
+
+    public User toEntity(UserCreateRequest request) {
+
+        if (request == null) {
+            return null;
+        }
+
+        User user = new User();
+        user.setName(request.name());
+        user.setCpf(request.cpf());
+        return user;
+    }
+
+
+    public UserResponse toResponse(User user) {
+
+        if (user == null) {
+            return null;
+        }
+
+        return new UserResponse(
+                user.getUserId(),
+                user.getName(),
+                user.getCpf()
+        );
+    }
+
+}
