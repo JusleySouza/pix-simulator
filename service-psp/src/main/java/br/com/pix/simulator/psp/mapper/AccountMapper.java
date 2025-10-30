c9cc439033c339fd4a855d923dbd297e65e9a8c7
Jusley Souza <jusleysouza31@gmail.com>
2025-10-30 18:05:01 -0300

    feat(account-mapper) Creating and implementing the AccountMapper mapping class

---
+++ service-psp/src/main/java/br/com/pix/simulator/psp/mapper/AccountMapper.java
@@ -0,0 +1,65 @@
+package br.com.pix.simulator.psp.mapper;
+
+import br.com.pix.simulator.psp.dto.account.AccountCreateRequest;
+import br.com.pix.simulator.psp.dto.account.AccountResponse;
+import br.com.pix.simulator.psp.dto.balance.BalanceResponse;
+import br.com.pix.simulator.psp.model.Account;
+import br.com.pix.simulator.psp.model.Psp;
+import br.com.pix.simulator.psp.model.User;
+import jakarta.persistence.EntityManager;
+import lombok.Generated;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+@Generated
+public class AccountMapper {
+
+    @Autowired
+    private EntityManager entityManager;
+
+    public Account toEntity(AccountCreateRequest request) {
+
+        if (request == null) {
+            return null;
+        }
+
+        Account account = new Account();
+        account.setPsp(entityManager.getReference(Psp.class, request.pspId()));
+        account.setUser(entityManager.getReference(User.class, request.userId()));
+        account.setAgency(request.agency());
+        account.setAccountNumber(request.accountNumber());
+        account.setBalance(request.initialBalance());
+        return account;
+    }
+
+
+    public AccountResponse toResponse(Account account) {
+
+        if (account == null) {
+            return null;
+        }
+        return new AccountResponse(
+                account.getAccountId(),
+                account.getPsp() != null ? account.getPsp().getBankName() : null,
+                account.getUser() != null ? account.getUser().getName() : null,
+                account.getAgency(),
+                account.getAccountNumber(),
+                account.getBalance()
+        );
+    }
+
+    public BalanceResponse toBalanceResponse(Account account) {
+
+        if (account == null) {
+            return null;
+        }
+        return new BalanceResponse(
+                account.getAccountId(),
+                account.getBalance()
+        );
+    }
+
+}
