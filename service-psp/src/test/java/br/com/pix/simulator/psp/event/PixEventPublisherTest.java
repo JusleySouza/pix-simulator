60aae6d2854efd22c555c591dd4b35e641146398
Jusley Souza <jusleysouza31@gmail.com>
2025-11-10 11:23:16 -0300

    test(pix-event-publisher) Creating the test class for the PixEventPublisher and adding a test to successfully publish a debit event.

---
+++ service-psp/src/test/java/br/com/pix/simulator/psp/event/PixEventPublisherTest.java
@@ -0,0 +1,44 @@
+package br.com.pix.simulator.psp.event;
+
+import br.com.pix.simulator.psp.config.RabbitMQConfig;
+import br.com.pix.simulator.psp.dto.event.TransactionEventResponse;
+import br.com.pix.simulator.psp.message.PixEventPublisher;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+
+import java.util.UUID;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class PixEventPublisherTest {
+
+    @Mock
+    private RabbitTemplate rabbitTemplate;
+
+    @InjectMocks
+    private PixEventPublisher pixEventPublisher;
+
+    private final UUID transactionId = UUID.randomUUID();
+
+    @Test
+    @DisplayName("You must publish the debit event that was correctly performed.")
+    void publishDebitMade_ShouldCallRabbitTemplate() {
+        TransactionEventResponse response = new TransactionEventResponse(transactionId, true, null);
+
+        pixEventPublisher.publishDebitMade(response);
+
+        verify(rabbitTemplate, times(1)).convertAndSend(
+                RabbitMQConfig.EXCHANGE_NAME,
+                RabbitMQConfig.RK_DEBIT_MADE,
+                response
+        );
+    }
+
+}
