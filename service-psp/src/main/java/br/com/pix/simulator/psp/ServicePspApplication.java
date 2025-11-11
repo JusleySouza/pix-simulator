8d6b67340cc4190b5d7125d86fccf6695ed0f40b
Jusley Souza <jusleysouza31@gmail.com>
2025-11-11 19:19:43 -0300

    refactor(main) adding @generated annotation.

--- service-psp/src/main/java/br/com/pix/simulator/psp/ServicePspApplication.java
+++ service-psp/src/main/java/br/com/pix/simulator/psp/ServicePspApplication.java
@@ -1,9 +1,11 @@
 package br.com.pix.simulator.psp;
 
+import lombok.Generated;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Generated
 public class ServicePspApplication {
 
 	public static void main(String[] args) {
