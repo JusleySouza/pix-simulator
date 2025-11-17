5c3bf7a05f4ba1413f93e19da1e7f0d671dfbad8
Jusley Souza <jusleysouza31@gmail.com>
2025-11-17 17:07:11 -0300

    feat(account-repository) Adding the query to search for an account by account ID, associated user ID, and associated PSP ID.

--- service-psp/src/main/java/br/com/pix/simulator/psp/repository/AccountRepository.java
+++ service-psp/src/main/java/br/com/pix/simulator/psp/repository/AccountRepository.java
@@ -2,7 +2,19 @@ package br.com.pix.simulator.psp.repository;
 
 import br.com.pix.simulator.psp.model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
 import java.util.UUID;
 
-public interface AccountRepository extends JpaRepository<Account, UUID> { }
+public interface AccountRepository extends JpaRepository<Account, UUID> {
+
+    @Query("SELECT a FROM Account a JOIN FETCH a.user u JOIN FETCH a.psp p WHERE a.accountId = :accountId AND u.userId = :userId AND p.pspId = :pspId")
+    Optional<Account> findByValidationKeys(
+            @Param("accountId") UUID accountId,
+            @Param("userId") UUID userId,
+            @Param("pspId") UUID pspId // Tipo ajustado para UUID
+    );
+
+}
