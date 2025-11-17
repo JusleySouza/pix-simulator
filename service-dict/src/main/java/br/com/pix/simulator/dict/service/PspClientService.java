dad88408ed9cfa52b5e6b923fa6890ebdf5e5873
Jusley Souza <jusleysouza31@gmail.com>
2025-11-17 17:00:54 -0300

    feat(psp-client-service) Adding the service responsible for communicating with the service-psp

---
+++ service-dict/src/main/java/br/com/pix/simulator/dict/service/PspClientService.java
@@ -0,0 +1,52 @@
+package br.com.pix.simulator.dict.service;
+
+import br.com.pix.simulator.dict.config.LoggerConfig;
+import br.com.pix.simulator.dict.dto.KeyCreateRequest;
+import br.com.pix.simulator.dict.dto.PspAccountValidationResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class PspClientService {
+
+    private final RestTemplate restTemplate;
+
+    @Value("${psp.service.url}")
+    private String pspServiceUrl;
+
+    public PspClientService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public boolean validateAccount(KeyCreateRequest request) {
+        String url = String.format(
+                "%s/%s/valida?userId=%s&pspId=%s",
+                pspServiceUrl,
+                request.accountId(),
+                request.userId(),
+                request.pspId()
+        );
+
+        try {
+            LoggerConfig.LOGGER_PSP_CLIENT.info("Validating account on PSP: {}", url);
+            PspAccountValidationResponse response = restTemplate.getForObject(url, PspAccountValidationResponse.class);
+
+            if (response == null || !response.isValid()) {
+                LoggerConfig.LOGGER_PSP_CLIENT.warn("Account validation failed (invalid response from PSP).");
+                return false;
+            }
+
+            LoggerConfig.LOGGER_PSP_CLIENT.info("Account validation OK. User: {}", response.userName());
+            return true;
+
+        } catch (HttpClientErrorException.NotFound e) {
+            LoggerConfig.LOGGER_PSP_CLIENT.warn("Account validation failed. Reason: Account/User/PSP not found in service-psp.");
+            return false;
+        } catch (Exception e) {
+            LoggerConfig.LOGGER_PSP_CLIENT.error("Error while trying to validate account on service-psp.", e);
+            return false;
+        }
+    }
+}
