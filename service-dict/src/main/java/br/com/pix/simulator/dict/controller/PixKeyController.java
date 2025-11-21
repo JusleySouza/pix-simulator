6c85fc2bf00359b8711d73a56204158d6d535ee7
Jusley Souza <jusleysouza31@gmail.com>
2025-11-21 11:26:16 -0300

    feat(pix-key-controller) Creation and implementation of the PIX key controller class and addition of the createKey route.

---
+++ service-dict/src/main/java/br/com/pix/simulator/dict/controller/PixKeyController.java
@@ -0,0 +1,29 @@
+package br.com.pix.simulator.dict.controller;
+
+import br.com.pix.simulator.dict.dto.KeyCreateRequest;
+import br.com.pix.simulator.dict.dto.KeyResponse;
+import br.com.pix.simulator.dict.service.PixKeyService;
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/keys")
+public class PixKeyController {
+
+    private final PixKeyService pixKeyService;
+
+    public PixKeyController(PixKeyService pixKeyService) {
+        this.pixKeyService = pixKeyService;
+    }
+
+    @PostMapping
+    @Operation(summary = "Register a new Pix Key")
+    public ResponseEntity<KeyResponse> createKey(@Valid @RequestBody KeyCreateRequest request) {
+        KeyResponse response = pixKeyService.createKey(request);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+}
