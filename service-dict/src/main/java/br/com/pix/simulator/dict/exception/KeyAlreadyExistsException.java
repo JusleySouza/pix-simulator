c4edaacb6d12c50209d40005bb09256efdfb67d2
Jusley Souza <jusleysouza31@gmail.com>
2025-11-21 11:32:38 -0300

    feat(validation-exception) Adding a class for exception handling for existing keys.

---
+++ service-dict/src/main/java/br/com/pix/simulator/dict/exception/KeyAlreadyExistsException.java
@@ -0,0 +1,11 @@
+package br.com.pix.simulator.dict.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class KeyAlreadyExistsException extends RuntimeException {
+    public KeyAlreadyExistsException(String message) {
+        super(message);
+    }
+}
