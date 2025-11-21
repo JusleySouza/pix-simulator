cb8eeae15a644107bd605ffb394da43afc3e5063
Jusley Souza <jusleysouza31@gmail.com>
2025-11-21 11:20:49 -0300

    feat(psp-account-validation-response) Adding response attributes when validation is successful.

--- service-dict/src/main/java/br/com/pix/simulator/dict/dto/PspAccountValidationResponse.java
+++ service-dict/src/main/java/br/com/pix/simulator/dict/dto/PspAccountValidationResponse.java
@@ -1,7 +1,11 @@
 package br.com.pix.simulator.dict.dto;
 
+import java.util.UUID;
+
 public record PspAccountValidationResponse(
-        boolean isValid,
+        UUID accountId,
+        UUID userId,
+        UUID pspId,
         String userName,
         String pspName
 ) { }
